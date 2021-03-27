72e8a13e2908ca1925556d8bb1f374a580ce35ab
Dylan Ascencio <empressglatia@gmail.com>
2021-03-26 19:53:37 -0500

    Added the daytimer actor, which displays a clock UI element to the screen, along with the resources it requires.

---
+++ examples/daytimer.c
@@ -0,0 +1,167 @@
+#define RES_NAME "Daytimer"
+#define TIMER_BLO_NAME "daytimer.blo"
+
+#include "../vanilla_defines/ww_defines.h"
+#include "./daytimer.h"
+
+static void * _ctors SECTION(".ctors");
+static void * _dtors SECTION(".dtors");
+
+int daDayTimer__Create(DayTimer_class* this) {
+  // Load the archive.
+  PhaseState phaseState = dComIfG_resLoad(&this->mPhaseRequest, RES_NAME);
+  if (phaseState != cPhs_COMPLEATE_e) {
+    // Not finished loading yet, check again next frame.
+    return phaseState;
+  }
+  
+  // Load our resource archive and store a pointer to it
+  dRes_info_c* resInfo = dRes_control_c__getResInfo(RES_NAME, g_dComIfG_gameInfo.mResCtrl.mObjectInfo, 0x40);
+  this->mTimerResArc = resInfo->mpArchive;
+  
+  daDayTimer__CreateUI(this);
+  
+  return cPhs_COMPLEATE_e;
+}
+
+void daDayTimer__CreateUI(DayTimer_class* this) {
+  this->mClockUI = (J2DScreen*)JKernel__operator_new(sizeof(J2DScreen));
+  if (this->mClockUI != 0) {
+    // Rectangle that defines the bounds of our parent J2DPane.
+    // Covers the entire screen (640x480)
+    TBox2__float__ rect = {
+      .mTL.x = 0.0f,
+      .mTL.y = 0.0f,
+      .mBR.x = 640.0f,
+      .mBR.y = 480.0f
+    };
+    
+    // Create the parent J2DPane object for our UI elements.
+    J2DPane__J2DPane(&this->mClockUI->parent);
+    this->mClockUI->parent.mName = 0x726f6f74; // 0x726f6f74 = 'root'
+    this->mClockUI->parent.mPosTL = rect.mTL;
+    this->mClockUI->parent.mPosBR = rect.mBR;
+    this->mClockUI->field_0xcf = 0;
+    
+    // This is sort of a hack because this is a C toolkit. We need to manually set what vtable our
+    // UI element uses. Even though mClockUI is a J2DScreen object, and its parent is a J2DPane,
+    // we set the parent to use J2DScreen's vtable since J2DScreen is a child class of J2DPane.
+    this->mClockUI->parent.vtbl = &J2DScreen____vt;
+    
+    // Initialize our J2DScreen object with the BLO from our resource archive.
+    J2DScreen__set(this->mClockUI, TIMER_BLO_NAME, this->mTimerResArc);
+    
+    // Get a reference to the main textbox to update the time string (0x74696D57 = 'timW')
+    this->mTimeText = (J2DTextBox*)J2DPane__search((J2DPane*)this->mClockUI, 0x74696D57);
+    if (!this->mTimeText) {
+      OSReport("Failed to find time textbox!\n");
+    }
+    
+    this->mTimeText->mpFont->parent.field_0x5 = 1; // Set monospace flag
+    this->mTimeText->mpFont->parent.field_0x8 = 15; // Set monospace width to 15px
+    
+    // Get a reference to the dropshadow textbox to update the time string (0x74696D42 = 'timB')
+    this->mTimeTextShadow = (J2DTextBox*)J2DPane__search((J2DPane*)this->mClockUI, 0x74696D42);
+    if (!this->mTimeTextShadow) {
+      OSReport("Failed to find time shadow textbox!\n");
+    }
+    
+    this->mTimeTextShadow->mpFont->parent.field_0x5 = 1; // Set monospace flag
+    this->mTimeTextShadow->mpFont->parent.field_0x8 = 15; // Set monospace width to 15px
+  }
+}
+
+int daDayTimer__createSolidHeap_CB(DayTimer_class* this) {
+  return 1;
+}
+
+int daDayTimer__Execute(DayTimer_class* this) {  
+  int CurMin = dKy_getdaytime_minute();
+  
+  // The game returns the hour in 24hr format, so let's make it 12hr instead.
+  int CurHour = dKy_getdaytime_hour() % 12;
+  if (CurHour == 0) {
+    CurHour = 12;
+  }
+  
+  // Update the time strings via snprintf (like printf but writes to a char buffer)
+  MSL_C_PPCEABI_bare_H__snprintf(this->mTimeText->mpStringPtr, 6, "%02d:%02d", CurHour, CurMin);
+  MSL_C_PPCEABI_bare_H__snprintf(this->mTimeTextShadow->mpStringPtr, 6, "%02d:%02d", CurHour, CurMin);
+  
+  return 1;
+}
+
+int daDayTimer__IsDelete(DayTimer_class* this) {
+  return 1;
+}
+
+int daDayTimer__Delete(DayTimer_class* this) {
+  return 1;
+}
+
+int daDayTimer__Draw(DayTimer_class* this) {
+  this->mClockUI->field_0xcc = 1; // I don't know if this is necessary, not sure what it does
+  
+  // Draw the UI to the screen. Some things need to change with this:
+  // * It can cause crashes
+  // * It doesn't hide when the other UI elements do
+  // * It flickers sometimes
+  J2DScreen__draw(this->mClockUI, 0.0, 0.0, (J2DGrafContext*)g_dComIfG_gameInfo.field_0x5d00);
+  return 1;
+}
+
+/** REL LINK FUNCTIONS **/
+/** Called to link and unlink the REL. Don't change these! **/
+void _prolog() {
+  DynamicLink__ModuleConstructorsX(&_ctors);
+  DynamicLink__ModuleProlog();
+}
+
+void _epilog() {
+  DynamicLink__ModuleEpilog();
+  DynamicLink__ModuleDestructorsX(&_dtors);
+}
+
+void _unresolved() {
+  DynamicLink__ModuleUnresolved();
+}
+
+/** CONSTANTS **/
+profile_method_class l_daDayTimer_Method = {
+  .parent = {
+    .mpCreate = &daDayTimer__Create,
+    .mpDelete = &daDayTimer__Delete,
+    .mpExecute = &daDayTimer__Execute,
+    .mpIsDelete = &daDayTimer__IsDelete,
+    .mpDraw = &daDayTimer__Draw,
+  },
+  .mpUnkFunc1 = 0,
+  .mpUnkFunc2 = 0,
+  .mpUnkFunc3 = 0,
+};
+
+const f_pc_profile__Profile_Actor g_profile_DayTimer = {
+  .parent = {
+    .mLayerID = -3,
+    .mListID = 7, // Affects execution order of actors in a given frame. Lower numbers execute first.
+    .mListPrio = -3,
+    .mPName = 0xB5, // Actor ID
+    0,
+    0,
+    .mpMtd0 = &g_fpcLf_Method,
+    .mSize = sizeof(DayTimer_class),
+    .mSizeOther = 0,
+    .mDefaultParameters = 0,
+    .mpMtd1 = &g_fopAc_Method,
+  },
+  
+  .mDrawPriority = 0x9F,
+  0,
+  0,
+  .mpMtd2 = &l_daDayTimer_Method,
+  .mStatus = 0,
+  .mActorType = fopAc_ac_c__Type__Regular,
+  .mCullType = 0,
+  0,
+  0,
+};
---
+++ examples/daytimer.h
@@ -0,0 +1,27 @@
+/** A clock actor that displays the current in-game time. **/
+/** Adds its own UI element to the screen via a BLO file. **/
+typedef struct DayTimer_class {
+  fopAc_ac_c parent;
+  request_of_phase_process_class mPhaseRequest;
+  
+  JKRArchive* mTimerResArc; // Pointer to our resource archive (BLO, images)
+  J2DScreen* mClockUI; // Pointer to the base UI element for the clock
+  
+  J2DTextBox* mTimeText; // Pointer to the textbox that displays the main time text
+  J2DTextBox* mTimeTextShadow; // Pointer to the textbox that displays the time's dropshadow
+
+} DayTimer_class;
+
+/** INTERFACE FUNCTIONS **/
+/** Called directly by the game for construction, destruction, and updating. **/
+int daDayTimer__Create(DayTimer_class* this);
+int daDayTimer__Execute(DayTimer_class* this);
+int daDayTimer__Draw(DayTimer_class* this);
+int daDayTimer__IsDelete(DayTimer_class* this);
+int daDayTimer__Delete(DayTimer_class* this);
+
+/** CONSTRUCTION FUNCTIONS **/
+/** These set up our actor when it's first created. **/
+void daDayTimer__daDayTimer(DayTimer_class* this);
+int daDayTimer__createSolidHeap_CB(DayTimer_class* this);
+void daDayTimer__CreateUI(DayTimer_class* this);
