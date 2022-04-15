0bd82f05297012255a03070550cb7cf68f8336ae
LagoLunatic <LagoLunatic@users.noreply.github.com>
2022-04-15 14:32:32 -0400

    Fix python version in batch files

--- assemble.bat
+++ assemble.bat
@@ -1,3 +1,3 @@
-py -3.8 asm_api/assemble.py
+py -3 asm_api/assemble.py
 if %errorlevel% neq 0 exit /b %errorlevel%
-py -3.8 asmpatch.py "D:/WW/Vanilla ISOs/The Legend of Zelda - The Wind Waker.iso" "../Vanilla ISOs/WW ASM Patched"
+py -3 asmpatch.py "D:/WW/Vanilla ISOs/The Legend of Zelda - The Wind Waker.iso" "../Vanilla ISOs/WW ASM Patched"
--- build_examples.bat
+++ build_examples.bat
@@ -1,9 +1,9 @@
 
-py build_rel.py "./examples/thwomp.c" 0x58 g_profile_THWOMP --rels_arc "../Random Seeds/WW Random EnemyRandoTest/files/RELS.arc" --md5_check ebe4d8bfc8c836cccb8cff5d1f56cf54
+py -3 build_rel.py "./examples/thwomp.c" 0x58 g_profile_THWOMP --rels_arc "../Random Seeds/WW Random EnemyRandoTest/files/RELS.arc" --md5_check ebe4d8bfc8c836cccb8cff5d1f56cf54
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-py build_rel.py "./examples/switch_op.c" 0x1A0 g_profile_SwitchOperator --md5_check cd385ac9214bea7128830ba4a0616f49
+py -3 build_rel.py "./examples/switch_op.c" 0x1A0 g_profile_SwitchOperator --md5_check cd385ac9214bea7128830ba4a0616f49
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-py build_rel.py "./examples/npc_test.c" 0x58 g_profile_NPC_Test --rels_arc "../Random Seeds/WW Random EnemyRandoTest/files/RELS.arc" --md5_check 9811b281c84bf3ab5163de205e71a8c4
+py -3 build_rel.py "./examples/npc_test.c" 0x58 g_profile_NPC_Test --rels_arc "../Random Seeds/WW Random EnemyRandoTest/files/RELS.arc" --md5_check 9811b281c84bf3ab5163de205e71a8c4
 if %errorlevel% neq 0 exit /b %errorlevel%
