LagoLunatic/WW_Hacking_API

commit 430c8adf4a1ed71b0406ddad11b44bd726dbbc40

Fix more bugs with switch_op

build_examples.bat
@@ -2,7 +2,7 @@
 py build_rel.py "./examples/thwomp.c" 0x58 g_profile_THWOMP --rels_arc "../Random Seeds/WW Random EnemyRandoTest/files/RELS.arc" --md5_check 811b96a10639bbdaf22074838adf4494
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-py build_rel.py "./examples/switch_op.c" 0x1A0 g_profile_SwitchOperator --md5_check d0c61b283f2d5080e26fcab43fb4be33
+py build_rel.py "./examples/switch_op.c" 0x1A0 g_profile_SwitchOperator --md5_check 27b792d713c7955a96d3ce63ad06f913
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 py build_rel.py "./examples/npc_test.c" 0x58 g_profile_NPC_Test --rels_arc "../Random Seeds/WW Random EnemyRandoTest/files/RELS.arc" --md5_check 82d2078763efa3082ba5470ebf4920e5

examples/switch_op.c
@@ -67,14 +67,8 @@ int daSwOp_Execute(SwitchOperator_class* this) {
 void daSwOp__eventCheck(SwitchOperator_class* this) {
   if (this->mEventProgressState == 0) {
     if (this->mEventIndexToStart != -1) {
-      bool ownSwitchIsSet = dSv_info_c__isSwitch(&g_dComIfG_gameInfo.mSvInfo, this->mSwitchToSet, this->parent.mCurrent.mRoomNo);
-      if (ownSwitchIsSet) {
-        if (this->parent.mEvtInfo.mCommand == dEvt__ActorCommand__InDemo) {
-          this->mEventProgressState++;
-        } else {
-          // Start the event.
-          fopAcM_orderOtherEventId(&this->parent, this->mEventIndexToStart, this->mEVNTIndexToStart, 0xFFFF, 0, 1);
-        }
+      if (this->parent.mEvtInfo.mCommand == dEvt__ActorCommand__InDemo) {
+        this->mEventProgressState++;
       }
     }
   } else if (this->mEventProgressState == 1) {
@@ -96,6 +90,8 @@ void daSwOp__switchCheck(SwitchOperator_class* this) {
   if (this->isDisabled) {
     return;
   }
+  
+  bool switchIsAlreadySet = dSv_info_c__isSwitch(&g_dComIfG_gameInfo.mSvInfo, this->mSwitchToSet, this->parent.mCurrent.mRoomNo);
   
   u8 switchToCheck = this->mFirstSwitchToCheck;
   int numSet = 0;
@@ -135,14 +131,19 @@ void daSwOp__switchCheck(SwitchOperator_class* this) {
       // Condition no longer met. Reset the counter.
       this->mRemainingFramesToWait = this->mTotalFramesToWait;
     }
-  } else if (conditionMet) {
+  } else if (conditionMet && !switchIsAlreadySet) {
     // Set the switch.
     dSv_info_c__onSwitch(&g_dComIfG_gameInfo.mSvInfo, this->mSwitchToSet, this->parent.mCurrent.mRoomNo);
+    
+    if (this->mEventIndexToStart != -1) {
+      // Start the event.
+      fopAcM_orderOtherEventId(&this->parent, this->mEventIndexToStart, this->mEVNTIndexToStart, 0xFFFF, 0, 1);
+    }
     
     if (!this->mContinuous) {
       this->isDisabled = true;
     }
-  } else if (this->mContinuous) {
+  } else if (!conditionMet && switchIsAlreadySet && this->mContinuous) {
     dSv_info_c__offSwitch(&g_dComIfG_gameInfo.mSvInfo, this->mSwitchToSet, this->parent.mCurrent.mRoomNo);
   }
 }

commit 72575f44636a7295ce5bf8591f23059f54ebf010

Completely refactor switch_op
This fixes a number of bugs in the class and makes it function much more similarly to the vanilla andsw2 class by using states.

For example, commit 430c8adf4a1ed71b0406ddad11b44bd726dbbc40 introduced a bug where the SwOp would fail to start its event if a different event was ongoing when its switch was set. That's fixed.

Commit 0a573e5029eace69d0b01cf83deed17b227bdc31 had a different bug where the SwOp would replay the event every time you exited and re-entered the room. That's now also fixed.

build_examples.bat
@@ -2,8 +2,8 @@
 py build_rel.py "./examples/thwomp.c" 0x58 g_profile_THWOMP --rels_arc "../Random Seeds/WW Random EnemyRandoTest/files/RELS.arc" --md5_check 811b96a10639bbdaf22074838adf4494
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-py build_rel.py "./examples/switch_op.c" 0x1A0 g_profile_SwitchOperator --md5_check d0c61b283f2d5080e26fcab43fb4be33
+py build_rel.py "./examples/switch_op.c" 0x1A0 g_profile_SwitchOperator --md5_check afb511459b2cb5dcd3597a95109783d8
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-py build_rel.py "./examples/npc_test.c" 0x58 g_profile_NPC_Test --rels_arc "../Random Seeds/WW Random EnemyRandoTest/files/RELS.arc" --md5_check 82d2078763efa3082ba5470ebf4920e5
+py build_rel.py "./examples/npc_test.c" 0x58 g_profile_NPC_Test --rels_arc "../Random Seeds/WW Random EnemyRandoTest/files/RELS.arc" --md5_check 4f5f2b54a4b10b7f73d9790e6cfa7ee2
 if %errorlevel% neq 0 exit /b %errorlevel%

examples/switch_op.c
@@ -34,12 +34,21 @@ int daSwOp_Create(SwitchOperator_class* this) {
   // Initialize the counter.
   this->mRemainingFramesToWait = this->mTotalFramesToWait;
   
-  if (!this->mContinuous) {
+  if (this->mSwitchToSet == 0xFF) {
+    this->mState = SwOpState_Deactivated;
+  } else {
     bool switchIsAlreadySet = dSv_info_c__isSwitch(&g_dComIfG_gameInfo.mSvInfo, this->mSwitchToSet, this->parent.mCurrent.mRoomNo);
     if (switchIsAlreadySet) {
-      this->isDisabled = true;
+      if (this->mContinuous) {
+        this->mState = SwOpState_CheckConditionUnmet;
+      } else {
+        this->mState = SwOpState_Deactivated;
+      }
+    } else {
+      this->mState = SwOpState_CheckConditionMet;
     }
   }
+  
   
   return cPhs_COMPLEATE_e;
 }
@@ -57,46 +66,140 @@ int daSwOp_Draw(SwitchOperator_class* this) {
 }
 
 int daSwOp_Execute(SwitchOperator_class* this) {
-  daSwOp__eventCheck(this);
-  
-  daSwOp__switchCheck(this);
+  switch (this->mState) {
+    case SwOpState_CheckConditionMet:
+      daSwOp__conditionMetCheck(this);
+      break;
+    case SwOpState_Timer:
+      daSwOp__timerCountdown(this);
+      break;
+    case SwOpState_StartingEvent:
+      daSwOp__eventStartCheck(this);
+      break;
+    case SwOpState_DuringEvent:
+      daSwOp__eventEndCheck(this);
+      break;
+    case SwOpState_CheckConditionUnmet:
+      daSwOp__conditionUnmetCheck(this);
+      break;
+    case SwOpState_Deactivated:
+      // Do nothing.
+      break;
+  }
   
   return 1;
 }
 
-void daSwOp__eventCheck(SwitchOperator_class* this) {
-  if (this->mEventProgressState == 0) {
-    if (this->mEventIndexToStart != -1) {
-      bool ownSwitchIsSet = dSv_info_c__isSwitch(&g_dComIfG_gameInfo.mSvInfo, this->mSwitchToSet, this->parent.mCurrent.mRoomNo);
-      if (ownSwitchIsSet) {
-        if (this->parent.mEvtInfo.mCommand == dEvt__ActorCommand__InDemo) {
-          this->mEventProgressState++;
-        } else {
-          // Start the event.
-          fopAcM_orderOtherEventId(&this->parent, this->mEventIndexToStart, this->mEVNTIndexToStart, 0xFFFF, 0, 1);
-        }
+void daSwOp__conditionMetCheck(SwitchOperator_class* this) {
+  bool conditionMet = daSwOp__isCondition(this);
+  if (conditionMet) {
+    if (this->mTotalFramesToWait > 0) {
+      // If there's a delay to wait, let the timer state take care of setting the switch.
+      this->mState = SwOpState_Timer;
+    } else if (this->mEventIndexToStart != -1) {
+      // If there's an event to start, let the event state take care of setting the switch.
+      this->mState = SwOpState_StartingEvent;
+    } else {
+      // Otherwise, set the switch directly.
+      dSv_info_c__onSwitch(&g_dComIfG_gameInfo.mSvInfo, this->mSwitchToSet, this->parent.mCurrent.mRoomNo);
+      if (this->mContinuous) {
+        this->mState = SwOpState_CheckConditionUnmet;
+      } else {
+        this->mState = SwOpState_Deactivated;
       }
     }
-  } else if (this->mEventProgressState == 1) {
-    bool eventFinished = dEvent_manager_c__endCheck(&g_dComIfG_gameInfo.mPlay.mEventMgr, this->mEventIndexToStart);
-    if (eventFinished) {
-      // This flag indicates this event should be finished, and the game should go back to
-      // checking if any other events are ready to play.
-      g_dComIfG_gameInfo.mPlay.mEvtCtrl.mStateFlags |= 8;
-      this->mEventProgressState++;
-    }
-    return;
-  } else {
-    // Event already played?
-    return;
   }
 }
 
-void daSwOp__switchCheck(SwitchOperator_class* this) {
-  if (this->isDisabled) {
+void daSwOp__timerCountdown(SwitchOperator_class* this) {
+  if (this->mContinuous) {
+    bool conditionMet = daSwOp__isCondition(this);
+    if (!conditionMet) {
+      // Condition no longer met, something changed while we were waiting for the timer.
+      // Reset the counter and go back to checking if the condition is met.
+      this->mRemainingFramesToWait = this->mTotalFramesToWait;
+      this->mState = SwOpState_CheckConditionMet;
+      return;
+    }
+  }
+  
+  if (this->mRemainingFramesToWait <= 0) {
+    // Timer has finished counting down.
+    if (this->mEventIndexToStart != -1) {
+      // If there's an event to start, let the event state take care of setting the switch.
+      this->mState = SwOpState_StartingEvent;
+    } else {
+      // Otherwise, set the switch directly.
+      dSv_info_c__onSwitch(&g_dComIfG_gameInfo.mSvInfo, this->mSwitchToSet, this->parent.mCurrent.mRoomNo);
+      if (this->mContinuous) {
+        // For a SwOp that is both continuous and has a timer, we simply deactivate it once the
+        // countdown has successfully reached zero.
+        // This is so puzzles where you have to keep the condition met for a certain period of time
+        // don't reset after you have met the condition for that period of time.
+        this->mState = SwOpState_Deactivated;
+      }
+    }
     return;
   }
   
+  // Count down.
+  this->mRemainingFramesToWait--;
+}
+
+void daSwOp__eventStartCheck(SwitchOperator_class* this) {
+  if (this->parent.mEvtInfo.mCommand == dEvt__ActorCommand__InDemo) {
+    // The event has started. Now we have to wait for it to end.
+    this->mState = SwOpState_DuringEvent;
+    // We also set this SwOp's switch now.
+    // This allows us to know not to start this event again in the future if the room is reloaded.
+    dSv_info_c__onSwitch(&g_dComIfG_gameInfo.mSvInfo, this->mSwitchToSet, this->parent.mCurrent.mRoomNo);
+    return;
+  }
+  
+  if (this->mContinuous) {
+    bool conditionMet = daSwOp__isCondition(this);
+    if (!conditionMet) {
+      // Condition no longer met, something changed while we were waiting for the event.
+      // Stop trying to start the event and go back to checking if the condition is met.
+      this->mState = SwOpState_CheckConditionMet;
+      return;
+    }
+  }
+  
+  // Ask to start the event.
+  // We call this every frame until the event actually starts.
+  // This can take a while if, we're trying to start the event while another event is ongoing.
+  // Doing it this way guarantees that this event will play as soon as the other event has finished,
+  // with no delay between them.
+  fopAcM_orderOtherEventId(&this->parent, this->mEventIndexToStart, this->mEVNTIndexToStart, 0xFFFF, 0, 1);
+}
+
+void daSwOp__eventEndCheck(SwitchOperator_class* this) {
+  bool eventFinished = dEvent_manager_c__endCheck(&g_dComIfG_gameInfo.mPlay.mEventMgr, this->mEventIndexToStart);
+  if (eventFinished) {
+    if (this->mContinuous) {
+      this->mState = SwOpState_CheckConditionUnmet;
+    } else {
+      this->mState = SwOpState_Deactivated;
+    }
+    // Setting this flag indicates this event should be finished, and the game should go back to
+    // checking if any other events are ready to play.
+    g_dComIfG_gameInfo.mPlay.mEvtCtrl.mStateFlags |= 8;
+  }
+}
+
+void daSwOp__conditionUnmetCheck(SwitchOperator_class* this) {
+  // This state is only used if the "continuous" param is set to true.
+  bool conditionMet = daSwOp__isCondition(this);
+  if (!conditionMet) {
+    // Condition no longer met, something changed while we were waiting.
+    // Unset this SwOp's switch and go back to checking if the condition is met.
+    this->mState = SwOpState_CheckConditionMet;
+    dSv_info_c__offSwitch(&g_dComIfG_gameInfo.mSvInfo, this->mSwitchToSet, this->parent.mCurrent.mRoomNo);
+  }
+}
+
+bool daSwOp__isCondition(SwitchOperator_class* this) {
   u8 switchToCheck = this->mFirstSwitchToCheck;
   int numSet = 0;
   int numUnset = 0;
@@ -127,24 +230,7 @@ void daSwOp__switchCheck(SwitchOperator_class* this) {
     conditionMet = true;
   }
   
-  if (this->mRemainingFramesToWait > 0) {
-    if (conditionMet) {
-      // Count down as long as the condition is met.
-      this->mRemainingFramesToWait--;
-    } else {
-      // Condition no longer met. Reset the counter.
-      this->mRemainingFramesToWait = this->mTotalFramesToWait;
-    }
-  } else if (conditionMet) {
-    // Set the switch.
-    dSv_info_c__onSwitch(&g_dComIfG_gameInfo.mSvInfo, this->mSwitchToSet, this->parent.mCurrent.mRoomNo);
-    
-    if (!this->mContinuous) {
-      this->isDisabled = true;
-    }
-  } else if (this->mContinuous) {
-    dSv_info_c__offSwitch(&g_dComIfG_gameInfo.mSvInfo, this->mSwitchToSet, this->parent.mCurrent.mRoomNo);
-  }
+  return conditionMet;
 }
 
 

examples/switch_op.h
@@ -8,8 +8,19 @@ enum SwOpLogicalOperation {
   SwOp_XNOR=5,
 };
 
+enum SwOpState {
+  SwOpState_CheckConditionMet=0,
+  SwOpState_Timer=1,
+  SwOpState_StartingEvent=2,
+  SwOpState_DuringEvent=3,
+  SwOpState_CheckConditionUnmet=4,
+  SwOpState_Deactivated=5,
+};
+
 typedef struct SwitchOperator_class {
   fopAc_ac_c parent;
+  
+  enum SwOpState mState;
   
   enum SwOpLogicalOperation mOperation;
   bool mContinuous;
@@ -19,14 +30,10 @@ typedef struct SwitchOperator_class {
   u8 mNumSwitchesToCheck;
   
   u8 mEVNTIndexToStart;
-  // TODO: maybe have an event param called when the switch is unset, too?
   s16 mEventIndexToStart;
-  u8 mEventProgressState;
   
   u8 mTotalFramesToWait;
   u8 mRemainingFramesToWait;
-  
-  bool isDisabled;
 } SwitchOperator_class;
 
 int daSwOp_Create(SwitchOperator_class* this);
@@ -35,5 +42,10 @@ int daSwOp_Delete(SwitchOperator_class* this);
 int daSwOp_Draw(SwitchOperator_class* this);
 int daSwOp_Execute(SwitchOperator_class* this);
 
-void daSwOp__eventCheck(SwitchOperator_class* this);
-void daSwOp__switchCheck(SwitchOperator_class* this);
+void daSwOp__conditionMetCheck(SwitchOperator_class* this);
+void daSwOp__timerCountdown(SwitchOperator_class* this);
+void daSwOp__eventStartCheck(SwitchOperator_class* this);
+void daSwOp__eventEndCheck(SwitchOperator_class* this);
+void daSwOp__conditionUnmetCheck(SwitchOperator_class* this);
+
+bool daSwOp__isCondition(SwitchOperator_class* this);