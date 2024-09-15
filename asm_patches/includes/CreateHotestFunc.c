b1b4766dab5be40d8f2d4118ae81e491dfaf38dc
Praecipua <rainiolassi@gmail.com>
2024-09-15 05:58:56 +0300

    Fixed a bug
    Fixed a bug where multiple instances of hotest were created on stage sea.

--- asm_patches/includes/CreateHotestFunc.c
+++ asm_patches/includes/CreateHotestFunc.c
@@ -4,6 +4,16 @@ void CreateHotestFunc(){
      cXyz scale = {1.0f, 1.0f, 1.0f};
      cXyz position = {0.0f, 0.0f, 0.0f};;
      csXyz angle = {0.0f, 0.0f, 0.0f};
+     int *stageNo =(int *)0x803C53A4;
+     //int roomNo = dStage_roomControl_c__mStayNo;
+     static int has_run = 0;
+     if (*stageNo == 0){
+          if(!has_run){
+          fopAcM_create(0x00B5, 0, &position, dStage_roomControl_c__mStayNo, &angle, &scale, 0xFF, 0);
+          has_run = 1;
+          }
+     }
+     else{
      fopAcM_create(0x00B5, 0, &position, dStage_roomControl_c__mStayNo, &angle, &scale, 0xFF, 0);
+     }
 }
-
